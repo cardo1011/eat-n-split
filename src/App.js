d0e9745fbd957759ed70da4e9453786804a1fa3d
RicardoMunoz <ricardomunoz4250@gmail.com>
2025-01-20 10:22:35 -0800

    apparently git was intialized in the wrong place the whole time so now im actually commiting my first commit

---
+++ src/App.js
@@ -0,0 +1,108 @@
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friendObj={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friendObj }) {
+  return (
+    <li>
+      <img src={friendObj.image} alt={friendObj.name} />
+      <h3>{`${friendObj.name}`}</h3>
+      {friendObj.balance < 0 && (
+        <p className="red">
+          You owe {friendObj.name} ${Math.abs(friendObj.balance)}
+        </p>
+      )}
+      {friendObj.balance > 0 && (
+        <p className="green">
+          {friendObj.name} owes you ${Math.abs(friendObj.balance)}
+        </p>
+      )}
+      {friendObj.balance === 0 && <p>You and {friendObj.name} are even</p>}
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👥 Friend name</label>
+      <input type="text" />
+
+      <label>📸 Image Url</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split the bill with X</h2>
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🧍 Your expense</label>
+      <input type="text" />
+
+      <label>👭 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who's paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
+  );
+}
