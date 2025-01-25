7532e3635bc934ce7b56bf11c106b193bef1a5c6
RicardoMunoz <ricardomunoz4250@gmail.com>
2025-01-25 10:27:27 -0800

    created state variables to FormSplitBill that keep track of howthe bill is being split by the user and their friend.

--- src/App.js
+++ src/App.js
@@ -176,20 +176,41 @@ function FormAddFriend({ onAddFriend }) {
 
 // Passing selectedFriend prop in order to be able to access the selectedFriends values into the form.
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setwhoIsPaying] = useState("user");
+
   return (
     <form className="form-split-bill">
       <h2>{`Split the bill with ${selectedFriend.name}`}</h2>
       <label>💰 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>🧍 Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={paidByUser}
+        // To prevent negative values that make no sense, I check to see if the input is greater than the bill and update the state variable accordingly
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+      />
 
       <label>👭 {`${selectedFriend.name}'s expense`}</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>🤑 Who's paying the bill?</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setwhoIsPaying(Number(e.target.value))}
+      >
         <option value="user">You</option>
         <option value="friend">{`${selectedFriend.name}`}</option>
       </select>
